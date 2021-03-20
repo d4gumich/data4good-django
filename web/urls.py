c08e1cf408d580f0ce1c90bcd0d9e952bf278255
mbjacobs <39781011+mbjacobs@users.noreply.github.com>
2021-03-19 20:37:45 -0400

    Update styling when rendering logo. (#74)
    * Add cluster filtering, full summary view, fix vanishing query
    
    * Updates to home page and Chetah.
    
    * Image updates and Cheetah fix
    
    * Add bios, rename Chetah URL, clean up unused code.
    
    * Update logo.

--- web/urls.py
+++ web/urls.py
@@ -8,13 +8,9 @@ from django.views.generic import TemplateView
 app_name='web'
 urlpatterns = [
     path('', views.MainView.as_view(), name='all'),
-    path('organization', views.OrganizationView.as_view(), name='all'),
-    path('projects', views.ProjectsView.as_view(), name='projects'),
+    # path('projects', views.ProjectsView.as_view(), name='projects'),
     path('projects/cio4good', views.CIO4GoodView.as_view(), name='cio4good'),
-    path('projects/pdfparsing', views.PDFParsingView.as_view(), name='pdfparsing'),
-    path('projects/digitalidentification', views.DigitalIdentificationView.as_view(), name='digitalidentification'),
-    path('projects/refugees', views.RefugeesView.as_view(), name='refugees'),
-    path('datasets', views.DataSetsView.as_view(), name='all'),
-    path('contribute', views.ContributeView.as_view(), name='all'),
-    path('faq', views.FAQView.as_view(), name='all'),
+    path('projects/chetah', views.ChetahView.as_view(), name='chetah'),
+    # path('datasets', views.DataSetsView.as_view(), name='all'),
+    # path('contribute', views.ContributeView.as_view(), name='all'),
 ]
