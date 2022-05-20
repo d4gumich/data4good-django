318ea9468742bf09ed8c8952441858cb63a2e376
MJacobs <mariahja@umich.edu>
2022-05-20 17:16:06 +0000

    favicon added

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.static import serve
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app_name='home'
 urlpatterns = [
--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
-
+import os
 from django.urls import path
 from django.contrib import admin
+from django.views.static import serve
 from . import views
 from django.views.generic import TemplateView
 # from web.views import ContributeView
@@ -15,3 +16,12 @@ urlpatterns = [
     # path('datasets', views.DataSetsView.as_view(), name='all'),
     # path('contribute', views.ContributeView.as_view(), name='all'),
 ]
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# urlpatterns += [
+#     path('favicon.ico', serve, {
+#         'path': 'favicon.ico',
+#         'document_root': os.path.join(BASE_DIR, 'static/images'),
+#         }
+#     ),
+# ]
