63db9eccc3e411bbb52b82bd6dd670053b576124
Sidra Effendi <sidraeffendi@gmail.com>
2023-02-24 14:10:13 -0500

    added code for language disaster and report type detection. Also added separate code to extract medata from document

---
+++ web/lang_metadata.py
@@ -0,0 +1,45 @@
+#creating dataframe with columns for each metadata- (Title, Date)and Content, assigning ID to each PDF and its text extraction.
+def df_pdfs(meta, keywords):
+  unique_id = []
+  title_list = []
+  date_list = []
+  language_list = []
+  content_list = keywords
+
+  count=0
+  for d in metadata:
+    if 'title' in d:
+       title_list.append(d['title'])
+    else:
+      title_list.append(file_name[count][:-4]) #remove the '.pdf from file name'
+    count+=1
+  
+  # [title_list.append(d['title']) if 'title' in d else title_list.append('null') for d in meta]
+  [date_list.append(d['date']) if 'date' in d else date_list.append('null') for d in meta]
+  [language_list.append(d['language']) if 'language' in d else language_list.append('null') for d in meta]
+  counter = 0
+  for i in meta:
+    unique_id.append(counter)
+    counter += 1
+  
+  df = pd.DataFrame()
+  df['ID'] = unique_id
+  df['Title'] = title_list
+  df['Date'] = date_list
+  df['Content'] = content_list
+  df['Language'] = language_list
+  df['file_path'] = item_list
+
+
+  return df
+
+
+ import spacy
+from spacy_langdetect import LanguageDetector
+nlp = spacy.load("en")
+nlp.add_pipe(LanguageDetector(), name="language_detector", last=True)
+nlp.max_length = 1500000
+df_lang['lang'] = df_lang['Content'].apply(lambda x: nlp(x)._.language)
+df_lang['lang1'] = df_lang.lang.apply(lambda x: x.get('language'))
+final_df = df_lang[df_lang["lang1"] == 'en']
+
---
+++ web/report_type.py
@@ -0,0 +1,35 @@
+#@author Sidra Effendi
+
+def check_pattern_present(pattern_list, doc_title):
+	for pattern in pattern_list:
+		if pattern.lower() in doc_title.lower():
+			return True
+		else: 
+			return False
+
+
+def detect_report_type(doc_title):
+	''' detect report type by looking at the title
+	'''
+	sitrep_pattern = ["situation","sitrep","situation overview"]
+	news_report_pattern = ["news","press release","newsletter"]
+	financial_pattern = ["financial","finance ","budget", "tax"]
+	annual_pattern = ['annual']
+
+
+	# True if pattern in doc_title.lower() for pattern in sitrep_pattern
+	if  check_pattern_present(sitrep_pattern, doc_title):
+		report_type = 'Situation Report'
+	elif check_pattern_present(news_report_pattern, doc_title):
+		report_type = 'News Report'
+	elif check_pattern_present(financial_pattern, doc_title):
+		report_type = 'Finance Report'
+	elif check_pattern_present(annual_pattern, doc_title):
+		report_type = 'Annual Report'
+	else:
+		report_type = None
+
+	return report_type
+
+
+# print(detect_report_type('2019-Airlink-Annual-Report'))
