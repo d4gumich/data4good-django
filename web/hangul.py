a0f1901d3794276fc14f917f8adf56ab4a29c29b
Sidra Effendi <sidraeffendi@gmail.com>
2023-03-06 16:33:58 -0500

    contents is now a list of first 4 pages if available; added title and summary section as well

--- web/hangul.py
+++ web/hangul.py
@@ -6,6 +6,9 @@ from .disaster_detection import get_disasters
 from collections import Counter
 from .get_file_metadata import extract_metadata
 from .location_detection import detected_potential_countries
+import re
+import html2text
+# from spacy_langdetect import LanguageDetector
 # from .report_type import detect_report_type
 
 nlp = spacy.load('en_core_web_sm')
@@ -19,8 +22,8 @@ def extract_pdf_content(pdf_path, content_as_pages):
         text_pages = body_without_tag.split("""<div class="page">""")[1:]
         num_pages = len(text_pages)
         print(num_pages)
-        if num_pages==int(raw_xml['metadata']['xmpTPg:NPages']) : #check if it worked correctly
-            for i in range(5):
+        if num_pages==int(raw_xml['metadata']['xmpTPg:NPages']) : 
+            for i in range(3):
             # for i in range(num_pages):
                 print('page number: '+ str(i+1))
                 print(text_pages[i].replace("\n", ""))
@@ -31,8 +34,57 @@ def extract_pdf_content(pdf_path, content_as_pages):
         pdf_content= parsed_pdf["content"].replace("\n", "")
     return pdf_content
 
+def get_content_pages(pdf_path):
+    raw_xml = parser.from_file(pdf_path, xmlContent=True)
+    body = raw_xml['content'].split('<body>')[1].split('</body>')[0]
+    body_without_tag = body.replace("<p>", "").replace("</p>", "\n").replace("<div>", "").replace("</div>","\n").replace("<p />","\n")
+    text_pages = body_without_tag.split("""<div class="page">""")[1:]
+    num_pages = len(text_pages)
+    pages_content=[]
+    if num_pages==int(raw_xml['metadata']['xmpTPg:NPages']) : 
+        # for i in range(5):
+        for i in range(num_pages):
+    #         # print('page number: '+ str(i+1))
+            h = html2text.HTML2Text()
+            h.ignore_links = True
+            # h.escape_all = True
+            text_pages[i]= h.handle(text_pages[i].replace("\n", " "))
+            text_pages[i] = re.sub(r'\s*(?:https?://)?www\.\S*\.[A-Za-z]{2,5}\s*', ' ', text_pages[i].replace("\n", " ")).strip()
+    #         text_pages[i] =re.sub(r'(<a href)(.+?)(/a&gt;)','',text_pages[i].replace("\n", ""))
+    #         # text_pages[i] = ''.join(re.findall('(<a href)(.+?)(/a&gt;)', ))
+            pages_content.append(text_pages[i])
+        pdf_content = pages_content
+    return pdf_content
+    # h = html2text.HTML2Text()
+    # h.ignore_links = True
+    # h.escape_all = True
+    # return h.handle(body)
 
-def extract_pdf_data(file_paths, want_metadata=True, want_content=False, content_as_pages=True):
+
+def get_doc_title(first_three_pages, metadata):
+
+    char_per_page_list = list(map(int, metadata['charsPerPage'][:3]))
+    mi = min(char_per_page_list)
+    indexes = [index for index in range(len(char_per_page_list)) if char_per_page_list[index] == mi]
+
+    return first_three_pages[indexes[0]]
+
+def get_doc_summary(first_six_pages):
+    # char_per_page_list = list(map(int, metadata['charsPerPage'][:6]))
+    check_str = 'summary'
+    summary = 'N/A'
+    for page_content in first_six_pages:
+        if check_str in page_content.lower():
+            summary=  page_content
+    return summary
+
+
+
+def clean_doc_content(content):
+    return content.replace("\n", "")
+
+
+def extract_pdf_data(file_paths, want_metadata=True, want_content=False):
     '''Given a list of path to PDFs, iterate over the list,
      and for each string, read in the PDF form its path and 
      return extracted text.
@@ -67,27 +119,68 @@ def extract_pdf_data(file_paths, want_metadata=True, want_content=False, content
             pdf['metadata'] = extracted_pdf_metadata
 
         if want_content:
-            extracted_pdf_content = extract_pdf_content(
-                file_path, content_as_pages)
-            pdf['content'] = extracted_pdf_content
+            pdf['content'] = parsed_pdf["content"]
 
         data_of_pdfs.append(pdf)
 
     return data_of_pdfs
 
+
+# def extract_pdf_data(file_paths, want_metadata=True, want_content=False, content_as_pages=True):
+#     '''Given a list of path to PDFs, iterate over the list,
+#      and for each string, read in the PDF form its path and 
+#      return extracted text.
+
+#      The flags might be changed during further development. 
+#      Right now they are designed to help in the process of 
+#      development and debugging.
+#      Developing what details about the document we want to 
+#      look at closely - metadata or content or both.
+#      It also returns content as pages so that we can decide 
+#      which pages to target going forward for information.
+
+#      @type list_of_path_to_pdf: list of string - [str1, str2]
+#      @param list_of_path_to_pdf: path of the pdf file to be read
+#      @type want_metadata: boolean
+#      @param want_metadata: Gets metadata about a document - default value True
+#      @type want_content: boolean
+#      @param want_content: Gets all the content of the document - default value False
+#      @type content_as_pages: boolean
+#      @param content_as_pages: Gets the content of the documents as pages else as a single text blob- default value True
+#      @rtype: List of dictionaries - [{metadata:'', 'content: ''}, {metadata:'', 'content: ''}, {metadata:'', 'content: ''}]
+#      @return:  For each document we get its metadata or content or both
+
+#     '''
+#     data_of_pdfs = []
+#     for file_path in file_paths:
+#         pdf = {}
+#         parsed_pdf = parser.from_file(file_path)
+
+#         if want_metadata:
+#             extracted_pdf_metadata = extract_metadata(parsed_pdf["metadata"])
+#             pdf['metadata'] = extracted_pdf_metadata
+
+#         if want_content:
+#             extracted_pdf_content = extract_pdf_content(
+#                 file_path, content_as_pages)
+#             pdf['content'] = extracted_pdf_content
+
+#         data_of_pdfs.append(pdf)
+
+#     return data_of_pdfs
+
     # return metadata,results
 
-# def extract_summary(content,text):
+# def detect_language(content):
+    
+# nlp.add_pipe("language_detector")
+# nlp = spacy.load("en")
+# nlp.add_pipe(LanguageDetector(), name="language_detector", last=True)
+# nlp.max_length = 1500000
+# df_lang['lang'] = df_lang['Content'].apply(lambda x: nlp(x)._.language)
+# df_lang['lang1'] = df_lang.lang.apply(lambda x: x.get('language'))
+# final_df = df_lang[df_lang["lang1"] == 'en']
 
-
-# def detect_location(content):
-#     nlped = nlp(content)
-	
-#     locations = [(x.text.replace('\n', ''), x.label_)
-#                  for x in nlped.ents if x.label_ == 'GPE']
-#     # most_common = [(x, z) for ((x, y), z) in Counter(locations).most_common()]
-#     most_common_location = Counter(locations).most_common(3)
-#     return most_common_location
 
 
 def run_hangul(file_path):
@@ -95,21 +188,39 @@ def run_hangul(file_path):
     init()
     # file_path ='./1.pdf'
     metadata_of_pdfs = extract_pdf_data(
-        [file_path], want_content=True, content_as_pages=False)
+        [file_path], want_content=True)
     # print(metadata_of_pdfs[0]['metadata'])
-    locations = detected_potential_countries(metadata_of_pdfs[0]['content'])
-    disasters = get_disasters(metadata_of_pdfs[0]['content'])
-    # doc_report_type = detect_report_type(metadata_of_pdfs[0]['metadata']['File name'])
+    # print(metadata_of_pdfs[0]['content'])
+    clean_doc_content = metadata_of_pdfs[0]['content'] #This is what was used throughout the document
+    # print(clean_doc_content, metadata_of_pdfs[0]['metadata'])
+    content_as_pages = get_content_pages(file_path)
+    if len(content_as_pages) <6:
+        doc_title  = get_doc_title(content_as_pages, metadata_of_pdfs[0]['metadata'])
+        doc_summary = get_doc_summary(content_as_pages)
+    else:
+        doc_title  =  get_doc_title(content_as_pages[:3],metadata_of_pdfs[0]['metadata'])
+        doc_summary = get_doc_summary(content_as_pages[:6])
+
+    locations = detected_potential_countries(clean_doc_content)
+    disasters = get_disasters(clean_doc_content)
+    # doc_report_type = detect_report_type(doc_title)
     # doc_report_type = detect_report_type(file_path)
+    if len(content_as_pages) <4:
+        display_content =content_as_pages
+    else:
+        display_content = content_as_pages[:4]
 
     return {
-        'metadata': metadata_of_pdfs,
+        'metadata': metadata_of_pdfs[0]['metadata'],
+        'Document Title':doc_title,
+        'Document Summary':doc_summary,
+        'content': display_content,
         'locations': locations,
         'disasters': disasters,
         # 'report_type':doc_report_type,
 
     }
-    # print(metadata_of_pdfs)
+    
     # 	extract_summary(content,text)
     #
     # separate the content and metadat and process accordingly
@@ -121,3 +232,4 @@ def init():
     # Start running the tika service
     tika.initVM()
 
+# print(run_hangul(''))
